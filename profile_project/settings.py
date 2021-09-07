91a3e476703b7426f1d0bcd566119047c21bf917
steven brand <steven.brand1999@gmail.com>
2021-09-06 23:35:46 -0500

    configured for deployment

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mcdks2!+!kafnl#c$i!bh!f0a$(l4hq++at1a$=4i+5*-=)nlx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = ['*']
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profile_api.UserProfile'
+
+STATIC_ROOT = 'static/'
